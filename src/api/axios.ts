363d0d5e40265046ad1a0a3a69907afe3db9a2cb
Herdiansyh <mohammadherdiansyah84@gmail.com>
2026-06-23 09:32:04 +0700

    perubahan axios url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,8 +5,7 @@ import type {
   AxiosResponse,
 } from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
